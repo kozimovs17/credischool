5baf68fd584d4fc9d4edabd5220601ac744839d0
kozimov_17 <kozimov_17@mail.ru>
2023-11-17 19:56:58 +0500

    v2.1

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
+let body = document.querySelector("body")
 let lines = document.querySelector(".lines");
 let navbar = document.querySelector(".navbar");
 let btns = document.querySelector(".btns");
-let ul = document.querySelectorAll("ul")
+
 let n = 0;
 lines.addEventListener("click", () =>{
     n++;
@@ -12,5 +13,90 @@ lines.addEventListener("click", () =>{
     }else{
         navbar.style.display = "block";
         btns.style.display = "block";  
+    }
+})
+
+let a = document.querySelectorAll("a")
+console.log(a);
+
+a.forEach((item) =>{
+    item.addEventListener("click", () =>{
+        if(item.dataset.link == "home"){
+            a[0].style.color = "#030F4B"
+            a[5].style.color = "#030F4B"
+            a[1].style.color = "#808396"
+            a[2].style.color = "#808396"
+            a[3].style.color = "#808396"
+            a[4].style.color = "#808396"
+            a[6].style.color = "#808396"
+            a[7].style.color = "#808396"
+            a[8].style.color = "#808396"
+            a[9].style.color = "#808396"
+        }
+        if(item.dataset.link == "campus"){
+            a[0].style.color = "#808396"
+            a[1].style.color = "#030F4B"
+            a[6].style.color = "#030F4B"
+            a[2].style.color = "#808396"
+            a[3].style.color = "#808396"
+            a[4].style.color = "#808396"
+            a[5].style.color = "#808396"
+            a[7].style.color = "#808396"
+            a[8].style.color = "#808396"
+            a[9].style.color = "#808396"
+        }
+        if(item.dataset.link == "education"){
+            a[0].style.color = "#808396"
+            a[1].style.color = "#808396"
+            a[2].style.color = "#030F4B"
+            a[7].style.color = "#030F4B"
+            a[3].style.color = "#808396"
+            a[4].style.color = "#808396"
+            a[5].style.color = "#808396"
+            a[6].style.color = "#808396"
+            a[8].style.color = "#808396"
+            a[9].style.color = "#808396"
+        }
+        if(item.dataset.link == "alumni"){
+            a[0].style.color = "#808396"
+            a[1].style.color = "#808396"
+            a[2].style.color = "#808396"
+            a[3].style.color = "#030F4B"
+            a[8].style.color = "#030F4B"
+            a[4].style.color = "#808396"
+            a[5].style.color = "#808396"
+            a[6].style.color = "#808396"
+            a[7].style.color = "#808396"
+            a[9].style.color = "#808396"
+        }
+        if(item.dataset.link == "about"){
+            a[0].style.color = "#808396"
+            a[1].style.color = "#808396"
+            a[2].style.color = "#808396"
+            a[3].style.color = "#808396"
+            a[9].style.color = "#030F4B"
+            a[4].style.color = "#030F4B"
+            a[5].style.color = "#808396"
+            a[6].style.color = "#808396"
+            a[7].style.color = "#808396"
+            a[8].style.color = "#808396"
+        }
+    })
+});
+
+let den = document.querySelector(".den");
+let circle = document.querySelector(".circle");
+let x = 0;
+
+den.addEventListener("click", () =>{
+    x++;
+    if(x % 2 !=0){
+        den.style.justifyContent = "flex-end";
+        circle.style.backgroundColor = "#423b3b";
+        body.style.backgroundColor = "#423b3b";
+    } else{
+        den.style.justifyContent = "flex-start";
+        circle.style.backgroundColor = "#fff";
+        body.style.backgroundColor = "#fff";
     }
 })
